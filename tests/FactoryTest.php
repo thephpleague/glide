94e72a750e7c5e1ae58094d2d3c1d3d05ec30e8e
Jonathan Reinink <jonathan@reinink.ca>
2014-12-30 21:56:00 -0500

    Mock file system when testing Factory class.

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -2,13 +2,15 @@
 
 namespace Glide;
 
+use Mockery;
+
 class FactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testCreateServer()
     {
         $server = Factory::server([
-            'source' => 'source',
-            'cache' => 'cache',
+            'source' => Mockery::mock('League\Flysystem\FilesystemInterface'),
+            'cache' => Mockery::mock('League\Flysystem\FilesystemInterface'),
         ]);
 
         $this->assertInstanceOf('Glide\Server', $server);
