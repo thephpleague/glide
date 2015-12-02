b543602cefe8e6f4af890ecb8a26a73e56a916b2
Andrew Carter <andrewcarter1992@gmail.com>
2015-12-02 11:43:07 +0000

    Added functional test for console binary

---
+++ tests/ConsoleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace League\Glide;
+
+class ConsoleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConsole()
+    {
+        exec('php ' . __DIR__ . '/../bin/console', $output);
+
+        $searchStrings = [
+            'Available commands:',
+            'help',
+            'list',
+            'sign-key-generator',
+            'Generate a key for signing image URLs'
+        ];
+
+        $buffer = implode(PHP_EOL, $output);
+
+        foreach ($searchStrings as $searchString) {
+            $this->assertContains($searchString, $buffer);
+        }
+    }
+}
