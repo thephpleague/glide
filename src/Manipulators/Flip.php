eb50baab0374e73a539ed096a54de0b55d4b8ec6
James Brooks <jbrooksuk@me.com>
2018-02-12 17:41:09 +0000

    Add flip Manipulator

---
+++ src/Manipulators/Flip.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace League\Glide\Manipulators;
+
+use Intervention\Image\Image;
+
+/**
+ * @property string $or
+ */
+class Flip extends BaseManipulator
+{
+    /**
+     * Perform flip image manipulation.
+     * @param  Image $image The source image.
+     * @return Image The manipulated image.
+     */
+    public function run(Image $image)
+    {
+        if ($flip = $this->getFlip()) {
+            return $image->flip($flip);
+        }
+
+        return $image;
+    }
+
+    /**
+     * Resolve flip.
+     * @return string The resolved flip.
+     */
+    public function getFlip()
+    {
+        if (in_array($this->flip, ['h', 'v'], true)) {
+            return $this->flip;
+        }
+    }
+}
