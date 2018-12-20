0b65dfee5e7dce1a08dfdb62fb360b3c8f392863
James Brooks <james@alt-three.com>
2018-12-20 16:23:34 +0000

    Add checks around all conditions of WebP

--- tests/Manipulators/EncodeTest.php
+++ tests/Manipulators/EncodeTest.php
@@ -44,19 +44,34 @@ class EncodeTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('image/jpeg', $this->manipulator->setParams(['fm' => 'jpg'])->run($this->jpg)->mime);
         $this->assertSame('image/jpeg', $this->manipulator->setParams(['fm' => 'jpg'])->run($this->png)->mime);
         $this->assertSame('image/jpeg', $this->manipulator->setParams(['fm' => 'jpg'])->run($this->gif)->mime);
-        $this->assertSame('image/jpeg', $this->manipulator->setParams(['fm' => 'jpg'])->run($this->webp)->mime);
+        // PHP 5.* may not be compiled with WebP support, so check first.
+        if (function_exists('imagecreatefromwebp')) {
+            $this->assertSame('image/jpeg', $this->manipulator->setParams(['fm' => 'jpg'])->run($this->webp)->mime);
+        }
+
         $this->assertSame('image/jpeg', $this->manipulator->setParams(['fm' => 'pjpg'])->run($this->jpg)->mime);
         $this->assertSame('image/jpeg', $this->manipulator->setParams(['fm' => 'pjpg'])->run($this->png)->mime);
         $this->assertSame('image/jpeg', $this->manipulator->setParams(['fm' => 'pjpg'])->run($this->gif)->mime);
-        $this->assertSame('image/jpeg', $this->manipulator->setParams(['fm' => 'pjpg'])->run($this->webp)->mime);
+        // PHP 5.* may not be compiled with WebP support, so check first.
+        if (function_exists('imagecreatefromwebp')) {
+            $this->assertSame('image/jpeg', $this->manipulator->setParams(['fm' => 'pjpg'])->run($this->webp)->mime);
+        }
+
         $this->assertSame('image/png', $this->manipulator->setParams(['fm' => 'png'])->run($this->jpg)->mime);
         $this->assertSame('image/png', $this->manipulator->setParams(['fm' => 'png'])->run($this->png)->mime);
         $this->assertSame('image/png', $this->manipulator->setParams(['fm' => 'png'])->run($this->gif)->mime);
-        $this->assertSame('image/png', $this->manipulator->setParams(['fm' => 'png'])->run($this->webp)->mime);
+        // PHP 5.* may not be compiled with WebP support, so check first.
+        if (function_exists('imagecreatefromwebp')) {
+            $this->assertSame('image/png', $this->manipulator->setParams(['fm' => 'png'])->run($this->webp)->mime);
+        }
+
         $this->assertSame('image/gif', $this->manipulator->setParams(['fm' => 'gif'])->run($this->jpg)->mime);
         $this->assertSame('image/gif', $this->manipulator->setParams(['fm' => 'gif'])->run($this->png)->mime);
         $this->assertSame('image/gif', $this->manipulator->setParams(['fm' => 'gif'])->run($this->gif)->mime);
-        $this->assertSame('image/gif', $this->manipulator->setParams(['fm' => 'gif'])->run($this->webp)->mime);
+        // PHP 5.* may not be compiled with WebP support, so check first.
+        if (function_exists('imagecreatefromwebp')) {
+            $this->assertSame('image/gif', $this->manipulator->setParams(['fm' => 'gif'])->run($this->webp)->mime);
+        }
 
         // PHP 5.* may not be compiled with WebP support, so check first.
         if (function_exists('imagecreatefromwebp')) {
