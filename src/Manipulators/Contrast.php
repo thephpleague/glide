8b5239b1b2ac3807aecb5ec34301c9c7c3c9ebf0
Art4 <art4@wlabs.de>
2024-01-22 15:46:37 +0100

    Update tests for Contrast manipulator

--- src/Manipulators/Contrast.php
+++ src/Manipulators/Contrast.php
@@ -12,11 +12,11 @@ class Contrast extends BaseManipulator
     /**
      * Perform contrast image manipulation.
      *
-     * @param Image $image The source image.
+     * @param ImageInterface $image The source image.
      *
-     * @return Image The manipulated image.
+     * @return ImageInterface The manipulated image.
      */
-    public function run(Image $image)
+    public function run(ImageInterface $image): ImageInterface
     {
         $contrast = $this->getContrast();
 
--- tests/Manipulators/ContrastTest.php
+++ tests/Manipulators/ContrastTest.php
@@ -2,6 +2,7 @@
 
 namespace League\Glide\Manipulators;
 
+use Intervention\Image\Interfaces\ImageInterface;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 
@@ -26,12 +27,12 @@ class ContrastTest extends TestCase
 
     public function testRun()
     {
-        $image = Mockery::mock('Intervention\Image\Image', function ($mock) {
+        $image = Mockery::mock(ImageInterface::class, function ($mock) {
             $mock->shouldReceive('contrast')->with('50')->once();
         });
 
         $this->assertInstanceOf(
-            'Intervention\Image\Image',
+            ImageInterface::class,
             $this->manipulator->setParams(['con' => 50])->run($image)
         );
     }
